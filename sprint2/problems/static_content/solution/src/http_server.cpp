9dafd7aec5dac7c665c67536ff1dc6942e8fa5c6
Indemion <mastrayane@mail.ru>
2024-11-01 10:52:05 +0300

    Испрпавление ошибок 3

--- sprint2/problems/static_content/solution/src/http_server.cpp
+++ sprint2/problems/static_content/solution/src/http_server.cpp
@@ -4,15 +4,15 @@
 
 namespace http_server {
 
+    inline void ReportError(beast::error_code ec, std::string_view what) {
+        std::cerr << what << ": "sv << ec.message() << std::endl;
+    }
+
     void SessionBase::Run() {
         // Вызываем метод Read, используя executor объекта stream_.
         // Таким образом вся работа со stream_ будет выполняться, используя его executor
         net::dispatch(stream_.get_executor(),
             beast::bind_front_handler(&SessionBase::Read, GetSharedThis()));
-    }
-
-    inline void ReportError(beast::error_code ec, std::string_view what) {
-        std::cerr << what << ": "sv << ec.message() << std::endl;
     }
 
     SessionBase::SessionBase(tcp::socket&& socket)
@@ -64,8 +64,6 @@ namespace http_server {
             ReportError(ec, "shutdown"sv);
         }
     }
-
-    
 
 }  // namespace http_server
 
